5a9082da34df30bdab764410c761e7dd50b9ab72
sergiocastillero <sergiocastillero9@gmail.com>
2023-03-07 18:53:40 +0000

    PRUEBA ELIMINAR LISTA

--- vista/main.php
+++ vista/main.php
@@ -1,32 +1,25 @@
 <h3>Benvingut a la pàgina principal del framework picat a pedra!</h3>
 <p>Dessitjo que sigui del teu gust</p>
-<table id="tareas">
+<table id="listas">
     <tr>
         <th>Id</th>
-        <th>Descripció</th>
-        <th>Data venciment</th>
-        <th>Realitzada</th>
-        <th>Llista</th>
+        <th>Nombre</th>
         <th>Eliminar</th>
     </tr>
 </table>
 <script>
-    function procesa_tareas(data){
+    function procesa_listas(data){
         for (var i=0;i < data.length; i++){
-            var row = document.getElementById("tareas").insertRow(1 + i);
+            var row = document.getElementById("listas").insertRow(1 + i);
             if (i%2==0){
                 row.classList.add("fila_parell");
             }else{
                 row.classList.add("fila_senar");
             }
             var cell_id = row.insertCell(0).innerHTML = data[i].ID;
-            var cell_descripcio = row.insertCell(1);
-            cell_descripcio.innerHTML = data[i].DESCRIPCION;
-            cell_descripcio.classList.add("cell_descripcio");
-            var cell_data_venciment = row.insertCell(2).innerHTML = data[i].FECHA_VENCIMIENTO;
-            var cell_realitzada = row.insertCell(3).innerHTML = data[i].REALIZADA;
-            var cell_llista = row.insertCell(4).innerHTML = data[i].LISTA_ID;
-            var cell_eliminar = row.insertCell(5);
+            var cell_nom = row.insertCell(1);
+            cell_nom.innerHTML = data[i].NOMBRE;
+            var cell_eliminar = row.insertCell(2);
             var eliminar_btn = document.createElement("button");
             eliminar_btn.innerHTML = "Eliminar";
             eliminar_btn.setAttribute("onclick", "eliminarLista(" + data[i].ID + ")");
@@ -35,9 +28,9 @@
     }
 
     function init(){
-        fetch("http://localhost/frmk/tareas/")
+        fetch("http://localhost/frmk/listas/")
             .then(response => response.json())
-            .then(data => procesa_tareas(data));
+            .then(data => procesa_listas(data));
     }
 
     function eliminarLista(id) {
