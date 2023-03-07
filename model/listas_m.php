df511ca808a0b3186f3fc8fdab143b0fae60fd97
sergiocastillero <sergiocastillero9@gmail.com>
2023-03-07 18:42:41 +0000

    pruebas para eliminar lista

--- model/listas_m.php
+++ model/listas_m.php
@@ -1,64 +1,62 @@
 <?php
-    Class Listas_model {
-        private $db;
-        private $listas;
-        public function __construct(){
-            require_once("./model/connexio.php");
-            $this->db=Connexio::connectar();
-            $this->listas=array();
-        }
-
-        public function getListas(){
-            $consulta = "SELECT * FROM LISTA";
-            $result = $this->db->query($consulta);
-            while ($fila=$result->fetch(PDO::FETCH_ASSOC)){
-                $this->listas[]=$fila;
-            }
-            return $this->listas;
-        }
-
-        public function getListaById($id){
-            $consulta = "SELECT * FROM LISTA WHERE ID =". $id .";";
-            $result = $this->db->query($consulta);
-            while ($fila=$result->fetch(PDO::FETCH_ASSOC)){
-                return $fila;
-            }
-            return null;
-        }
-
-        public function getListaByTarea($tarea_id){
-            $consulta = "SELECT * FROM LISTA WHERE ID = (SELECT LISTA_ID FROM TAREA WHERE ID=".$tarea_id.");";
-            $result = $this->db->query($consulta);
-            while ($fila=$result->fetch(PDO::FETCH_ASSOC)){
-                return $fila;
-            }
-            return null;
-        }
-
-        public function appendTarea($tarea){
-            $new_id = -1;
-            if ($tarea){
-                $consulta = "SELECT ID FROM TAREA ORDER BY ID DESC LIMIT 1;";
-                $result = $this->db->query($consulta);
-                $last_id = $result->fetch(PDO::FETCH_ASSOC)["ID"];
-                $new_id = $last_id + 1;
-                $consulta = "INSERT INTO TAREA (ID, DESCRIPCION, FECHA_VENCIMIENTO, REALIZADA, LISTA_ID) VALUES(:id, :descripcion, :fecha_vencimiento, :realizada, :lista_id);";
-                $dades = [
-                    'id'=>$new_id,
-                    'descripcion'=>$tarea->descripcion,
-                    'fecha_vencimiento'=>$tarea->fecha_vencimiento,
-                    'realizada'=>$tarea->realizada,
-                    'lista_id'=>$tarea->lista_id
-                ];
-                $res_insert = $this->db->prepare($consulta)->execute($dades);
-            }
-            return $new_id;
-        }
-
-        public function deleteListaById($id){
-            $consulta = "DELETE FROM LISTA WHERE ID=?;";
-                
-            $res_delete = $this->db->prepare($consulta)->execute(array($id));
-        }
+class Listas_model {
+    private $db;
+    private $listas;
+    public function __construct(){
+        require_once("./model/connexio.php");
+        $this->db=Connexio::connectar();
+        $this->listas=array();
     }
-?> 
+
+    public function getListas(){
+        $consulta = "SELECT * FROM LISTA";
+        $result = $this->db->query($consulta);
+        while ($fila=$result->fetch(PDO::FETCH_ASSOC)){
+            $this->listas[]=$fila;
+        }
+        return $this->listas;
+    }
+
+    public function getListaById($id){
+        $consulta = "SELECT * FROM LISTA WHERE ID = :id;";
+        $stmt = $this->db->prepare($consulta);
+        $stmt->bindParam(':id', $id, PDO::PARAM_INT);
+        $stmt->execute();
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
+
+    public function getListaByTarea($tarea_id){
+        $consulta = "SELECT * FROM LISTA WHERE ID = (SELECT LISTA_ID FROM TAREA WHERE ID=:tarea_id);";
+        $stmt = $this->db->prepare($consulta);
+        $stmt->bindParam(':tarea_id', $tarea_id, PDO::PARAM_INT);
+        $stmt->execute();
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
+
+    public function appendLista($lista){
+        $new_id = -1;
+        if ($lista){
+            $consulta = "SELECT ID FROM LISTA ORDER BY ID DESC LIMIT 1;";
+            $result = $this->db->query($consulta);
+            $last_id = $result->fetch(PDO::FETCH_ASSOC)["ID"];
+            $new_id = $last_id + 1;
+            $consulta = "INSERT INTO LISTA (ID, NOMBRE, USUARIO_ID) VALUES(:id, :nombre, :usuario_id);";
+            $dades = [
+                'id'=>$new_id,
+                'nombre'=>$lista->nombre,
+                'usuario_id'=>$lista->usuario_id
+            ];
+            $stmt = $this->db->prepare($consulta);
+            $stmt->execute($dades);
+        }
+        return $new_id;
+    }
+
+    public function deleteListaById($id){
+        $consulta = "DELETE FROM LISTA WHERE ID=:id;";
+        $stmt = $this->db->prepare($consulta);
+        $stmt->bindParam(':id', $id, PDO::PARAM_INT);
+        $stmt->execute();
+    }
+}
+?>
