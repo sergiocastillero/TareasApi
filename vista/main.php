710c5dda77c81ddfe3bb60ddd2490c5dbb40dd61
sergiocastillero <sergiocastillero9@gmail.com>
2023-03-07 17:51:00 +0000

    eliminar lista por id

--- vista/main.php
+++ vista/main.php
@@ -1,5 +1,16 @@
+<html>
+    <head>
+        <title>Framework picat a pedra</title>
+        <link rel="stylesheet" type="text/css" href="estils.css">
+    </head>
+    <body>
         <h3>Benvingut a la pàgina principal del framework picat a pedra!</h3>
         <p>Dessitjo que sigui del teu gust</p>
+        <div>
+            <label for="list-id">ID de la llista:</label>
+            <input type="text" id="list-id" name="list-id">
+            <button id="delete-list-button">Eliminar llista i tasques</button>
+        </div>
         <table id="tareas">
             <tr>
                 <th>Id</th>
@@ -7,6 +18,7 @@
                 <th>Data venciment</th>
                 <th>Realitzada</th>
                 <th>Llista</th>
+                <th>Accions</th>
             </tr>
         </table>
         <script>
@@ -25,16 +37,33 @@
                     var cell_data_venciment = row.insertCell(2).innerHTML = data[i].FECHA_VENCIMIENTO;
                     var cell_realitzada = row.insertCell(3).innerHTML = data[i].REALIZADA;
                     var cell_llista = row.insertCell(4).innerHTML = data[i].LISTA_ID;
+                    var cell_accions = row.insertCell(5);
+                    var delete_button = document.createElement("button");
+                    delete_button.innerHTML = "Eliminar";
+                    delete_button.addEventListener("click", function(){
+                        var tarea_id = row.cells[0].innerHTML;
+                        fetch("http://localhost/frmk/tareas/" + tarea_id, {
+                            method: "DELETE"
+                        }).then(() => location.reload());
+                    });
+                    cell_accions.appendChild(delete_button);
                 }
             }
-
             function init(){
-                fetch("http://localhost/frmk/tareas/")
-                    .then(response => response.json())
-                    .then(data => procesa_tareas(data));
+            fetch("http://localhost/frmk/tareas/")
+                .then(response => response.json())
+                .then(data => procesa_tareas(data));
             }
             setTimeout(init, 1000);
+
+            document.getElementById("delete-list-button").addEventListener("click", function(){
+                var list_id = document.getElementById("list-id").value;
+                if (list_id){
+                    fetch("http://localhost/frmk/llistes/" + list_id, {
+                        method: "DELETE"
+                    }).then(() => location.reload());
+                }
+            });
         </script>
     </body>
 </html>
-
