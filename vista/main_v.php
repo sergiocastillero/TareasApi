04641ea2f3f0b8fff98b1f3716065503a1ee7444
isard <sergiocastillero9@gmail.com>
2023-03-08 18:03:10 +0100

    mostrar todo en main

--- vista/main_v.php
+++ vista/main_v.php
@@ -1,5 +1,11 @@
-<h3>Benvingut a la pàgina principal del framework picat a pedra!</h3>
-<p>Dessitjo que sigui del teu gust</p>
+<h3>LLISTES</h3>
+<table id="listas">
+    <tr>
+        <th>Id</th>
+        <th>Nom</th>
+    </tr>
+</table>
+<h3>TASQUES</h3>
 <table id="tareas">
     <tr>
         <th>Id</th>
@@ -11,6 +17,7 @@
 </table>
 <script>
     function procesa_tareas(data){
+        console.log(data);
         for (var i=0;i < data.length; i++){
             var row = document.getElementById("tareas").insertRow(1 + i);
             if (i%2==0){
@@ -28,11 +35,35 @@
         }
     }
 
-    function init(){
+    function procesa_listas(data){
+        console.log(data);
+        for (var i=0;i < data.length; i++){
+            var row = document.getElementById("listas").insertRow(1 + i);
+            if (i%2==0){
+                row.classList.add("fila_parell");
+            }else{
+                row.classList.add("fila_senar");
+            }
+            var cell_id = row.insertCell(0);
+            cell_id.innerHTML = data[i].ID;
+            var cell_nom = row.insertCell(1);
+            cell_nom.innerHTML = data[i].NOMBRE;
+        }
+    }
+
+    function initTareas(){
         fetch("http://localhost/frmk/tareas/")
             .then(response => response.json())
             .then(data => procesa_tareas(data));
     }
 
-    setTimeout(init, 1000);
+    function initListas(){
+        fetch("http://localhost/frmk/listas/")
+            .then(response => response.json())
+            .then(data => procesa_listas(data));
+    }
+
+    setTimeout(initTareas, 1000);
+    setTimeout(initListas, 1000);
+
 </script>
